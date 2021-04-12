e5bbdfb9e8afc5d01a4a8225961c2f70e7e18792
Pjoot <52052022+Pjoot@users.noreply.github.com>
2021-04-12 18:50:45 +0200

    Bugfixing
    Game now runs again!

--- Generator_code/Supply_name_conv.py
+++ Generator_code/Supply_name_conv.py
@@ -5,28 +5,30 @@ import sys
 
 #Doc = 'localisation\\strategic_region_names_l_english.yml'
 Output = 'Generator_code\\Output\\'
-Source = 'map\\strategicregions\\'
+Source = 'map\\supplyareas\\'
 
 print("The script has started. Please do not touch any files")
 
 List = [filename for filename in os.listdir(Source)]
 
-TXT = "ï»¿l_english: \n"		#needs to have that weird starter to ensure it is a readable file
+TXT = "ï»¿l_english: \n"
 
 for region in List:
+	#with open(Source+region,'r') as Cont:
+	#	NAME = Cont.read().split('"')[1]
+	#	print(NAME)
 
-	ID = region.split("-",1)[0]						#find region ID
+	ID = region.split("-",1)[0]
 	Pre_NAME = region.split("-",1)[1]
-	NAME = Pre_NAME.split(".")[0]					#find file name
-
+	NAME = Pre_NAME.split(".")[0]
 	#STRATEGICREGION_4:0 "Ireland"
-	Entry = f' STRATEGICREGION_{ID}:0 "{NAME}" \n'	#put the ID and name into loc file
+	Entry = f' SUPPLYAREA_{ID}:0 "{NAME}" \n'
 	print(Entry)
 
 	TXT += Entry
 
-with open(Output+"strategic_region_names_l_english.yml", 'w') as DOC:
-	DOC.write(TXT)									#writes the file
+with open(Output+"supply_area_names_l_english.yml", 'w') as DOC:
+	DOC.write(TXT)
 DOC.close()
 
 print("- The script has ended. Thank you for using! -")
